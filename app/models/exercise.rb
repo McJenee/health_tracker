f80b35544404434aee0bfa4b5b833e9f03e16716
Llamamama <reneeq25@gmail.com>
2015-10-02 16:46:34 -0400

    Exercise/Test Validation Test Pass

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,9 @@
 class Exercise < ActiveRecord::Base
+belongs_to :exercise_type
+
+# def self.calories_burned
+#   if self.count == 0
+#     0
+#   else
+#     calories_now = self.
 end
--- app/models/weight.rb
+++ app/models/weight.rb
@@ -1,2 +1,6 @@
 class Weight < ActiveRecord::Base
+
+  def daily_weight
+  
+  end
 end
