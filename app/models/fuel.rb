271a140500449bd335fa596d759c6188020fe597
Llamamama <reneeq25@gmail.com>
2015-10-03 13:46:43 -0400

    Total steps

--- app/models/fuel.rb
+++ app/models/fuel.rb
@@ -1,8 +1,12 @@
 class Fuel < ActiveRecord::Base
 
-  def net_calories # need to work on this
+  def self.calories
+    self.all.reduce(0) { |s, c| s + c.calorie }
+  end
+  
+  def net_calories
     today = Time.now.strftime("%Y-%m-%d")
     @net_fuel = @self.today - @burn.today
   end
-  
+
 end
