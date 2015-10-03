bb4f813963886bcffdcd9dd57a6788047dc933ac
Josh McKelvey <jmckelvey0@gmail.com>
2015-10-03 14:19:55 -0400

    fix fuel and fuel_test

--- app/models/fuel.rb
+++ app/models/fuel.rb
@@ -3,5 +3,9 @@ class Fuel < ActiveRecord::Base
   def self.calories
     self.all.reduce(0) { |s, c| s + c.calorie }
   end
+  def net_calories
+    today = Time.now.strftime("%Y-%m-%d")
+    @net_fuel = @self.today - @burn.today
+  end
 
 end
--- test/models/fuel_test.rb
+++ test/models/fuel_test.rb
@@ -5,6 +5,18 @@ class FuelTest < ActiveSupport::TestCase
     assert true
   end
 
+  test "net calories work" do
+    f1 = Fuel.create!(calorie: "200", date: 2015-10-02)
+    f2 = Fuel.create!(calorie: "200", date: 2015-10-02)
+    f3 = Fuel.create!(calorie: "200", date: 2015-10-02)
+    f4 = Fuel.create!(calorie: "200", date: 2015-10-03)
+
+    b1 = Exercise.create!(burn: 50, date: 2015-10-02)
+    b2 = Exercise.create!(burn: 60, date: 2015-10-02)
+
+    assert net_calories = "490"
+  end
+
   test "total calories consumed" do
     Fuel.create(calorie: 2000, meal_type: "Dinner")
     Fuel.create(calorie: 500, meal_type: "Snack")
