e32699e92ad8f65e1d29704f107211b8c015076e
mamallama <reneeq25@gmail.com>
2015-10-04 19:00:43 -0400

    Add step

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,7 +2,9 @@ class Exercise < ActiveRecord::Base
 belongs_to :exercise_type
 
   def self.burned
-    self.all.reduce(0) { |s, c| s + c.burn }
+    e = self.all.reduce(0) { |s, c| s + c.burn }
+    s = Step.all.reduce(0) { |s, c| s + c.step} / 20
+    e + s
   end
 
   def self.daily_burn(date)
@@ -11,7 +13,10 @@ belongs_to :exercise_type
       "No data to calculate"
     else
       weight = Exercise.where(date: today).sum(:burn)
+      step_burn = Step.where(date: today).sum(:step)/20
+      weight + step_burn
     end
+
   end
 
 end
